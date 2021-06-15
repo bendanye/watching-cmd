4b01a4ad8a2d36d957f83d03a9e9422a6ca6df24
bendanye <shen_soonng@hotmail.com>
2021-06-15 09:06:38 +0800

    fix bug when parsing command line arg

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,13 @@ import { watch }  from './watch';
 program
   .version('1.0.0')
   .description('An application for watching directory and run command whenever detect got changes')
-  .requiredOption('-d, --directory', 'Directory to watch')
-  .requiredOption('-c, --command', 'Command to run')
+  .requiredOption('-d, --directory <directory>', 'Directory to watch')
+  .requiredOption('-c, --command <command>', 'Command to run')
   .parse(process.argv);
 
-const directory: string = !program.directory ? 'C:\\Users\\benjaminng\\Documents\\GitHub\\node-exploration': program.directory;
-const commandToExecute: string = !program.command ? 'npm run test': program.command;
+const options = program.opts();
+
+const directory: string = options.directory;
+const commandToExecute: string = options.command;
 
 watch(directory, commandToExecute);
