a9be8ebd3630c366a07f15464c33e0ffb8bfbaaf
benjaminng <shen_soonng@hotmail.com>
2021-06-20 11:28:02 +0800

    remove unused module and codes

--- src/sound.ts
+++ src/sound.ts
@@ -1,23 +1,12 @@
-//const player = require('play-sound')(opts = {});
+const sound = require('sound-play');
 
-const player = require('play-sound')();
-const sound = require("sound-play");
-
-const path = require('path')
-const SOUND_DIRECTORY = path.join(__dirname, "..", "sounds");
+const path = require('path');
+const SOUND_DIRECTORY = path.join(__dirname, '..', 'sounds');
 
 export const playSuccessSoundAsync = () => {
-    //playSoundAsync(`${SOUND_DIRECTORY}/success.wav`);
-    sound.play(path.join(SOUND_DIRECTORY, "success.wav"));
+    sound.play(path.join(SOUND_DIRECTORY, 'success.wav'));
 };
 
 export const playFailSoundAsync = () => {
-    //playSoundAsync(`${SOUND_DIRECTORY}/fail.wav`);
-    sound.play(path.join(SOUND_DIRECTORY, "fail.wav"));
-};
-
-const playSoundAsync = (soundName: string) => {
-    player.play(soundName, function (err: any) {
-        if (err) throw err;
-    });
+    sound.play(path.join(SOUND_DIRECTORY, 'fail.wav'));
 };
