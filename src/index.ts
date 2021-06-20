6151e09eecc2307d9bc4304d965b5c45ead17b0c
benjaminng <shen_soonng@hotmail.com>
2021-06-20 11:22:12 +0800

    added exclude config pattern by config id

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,37 @@
 const program = require('commander');
+const fs = require('fs');
 
-import { watch }  from './watch';
+import { watch } from './watch';
+import { log } from './log';
 
 program
-  .version('1.0.0')
-  .description('An application for watching directory and run command whenever detect got changes')
-  .requiredOption('-d, --directory <directory>', 'Directory to watch')
-  .requiredOption('-c, --command <command>', 'Command to run')
-  .parse(process.argv);
+    .version('1.0.0')
+    .description('An application for watching directory and run command whenever detect got changes')
+    .requiredOption('-d, --directory <directory>', 'Directory to watch')
+    .requiredOption('-c, --command <command>', 'Command to run')
+    .option('-i, --excludeConfigId <excludeConfigId>', 'Exclude Config Id to retrieve information of exclude_patterns')
+    .option('-e, --excludeConfigPath <excludeConfigPath>', 'Exclude Config Path')
+    .parse(process.argv);
 
 const options = program.opts();
 
 const directory: string = options.directory;
 const commandToExecute: string = options.command;
 
-watch(directory, commandToExecute);
+const excludeConfigPath: string = options.excludeConfigPath;
+const excludeConfigId: string = options.excludeConfigId;
+
+if (!excludeConfigId && !excludeConfigPath) {
+    watch(directory, commandToExecute, []);
+} else if (excludeConfigId && excludeConfigPath) {
+    const excludePatterns: Array<string> = getExcludePatterns(excludeConfigPath, excludeConfigId);
+    watch(directory, commandToExecute, excludePatterns);
+} else {
+    log('Both --excludeConfigId and --excludeConfigPath must be set when one of those is specified');
+}
+
+function getExcludePatterns(path: string, configId: string) {
+    const rawdata = fs.readFileSync(path);
+    const config = JSON.parse(rawdata);
+    return config[configId].excludePatterns;
+}
--- src/watch.ts
+++ src/watch.ts
@@ -6,25 +6,38 @@ import { executeCommand } from './execute_command';
 
 import { playSuccessSoundAsync, playFailSoundAsync } from './sound';
 
-export const watch = (directory: string, commandToExecute: string) => {
-    watching(directory, { recursive: true }, function (evt: any, updatedFileName: string) {
-        execute(directory, commandToExecute);
-    });
-    
-    log(`Start watching directory, ${directory}`);
-}
+export const watch = (directory: string, commandToExecute: string, excludePatterns: Array<string>) => {
+    watching(
+        directory,
+        {
+            recursive: true,
+            filter(f: string, skip: boolean) {
+                for (const excludePattern of excludePatterns) {
+                    if (f.includes(excludePattern)) return skip;
+                }
 
-const execute = async(directory: string, commandToExecute: string) => {
-    
+                return true;
+            },
+        },
+        function (evt: any, updatedFileName: string) {
+            log(updatedFileName);
+            execute(directory, commandToExecute);
+        },
+    );
+
+    log(`Start watching directory, ${directory} and excluding_patterns=${JSON.stringify(excludePatterns)}`);
+};
+
+const execute = async (directory: string, commandToExecute: string) => {
     const result = await executeCommand(directory, commandToExecute);
 
     log(result.message);
-    
-    if (result.type === "SUCCESS") {
+
+    if (result.type === 'SUCCESS') {
         playSuccessSoundAsync();
-        log(colors.green("✅  Command run successfully ✅"));
+        log(colors.green('✅  Command run successfully ✅'));
     } else {
         playFailSoundAsync();
-        log(colors.red("🚨  Command run with error 🚨"));
-    } 
-}
+        log(colors.red('🚨  Command run with error 🚨'));
+    }
+};
